6031cc75975be0f5f31e33905912fd87f02a0606
jaeseongDev <qkrwotjd1445@naver.com>
2020-12-05 04:09:32 +0900

    fix(Computer) : 서로 다른 임의의 수 3개 생성하는 기능에서 생성된 랜덤 값이 불변하도록 수정

--- src/main/java/baseball/Computer.java
+++ src/main/java/baseball/Computer.java
@@ -9,19 +9,24 @@ public class Computer {
     private static final int END_NUMBER_IN_RANGE = 9;
     private List<Integer> randomNumbers = new ArrayList<>();
 
+    public Computer() {
+        setRandomNumbers();
+    }
+
     // 1에서 9까지 서로 다른 임의의 수 3개를 생성
     public List<Integer> getRandomNumbers() {
+        return new ArrayList<>(randomNumbers);
+    }
+
+    private void setRandomNumbers() {
         // 숫자를 3개 생성할 때까지만 반복
         while (randomNumbers.size() != 3) {
             addRandomNumber();
         }
-        return randomNumbers;
     }
 
     private void addRandomNumber() {
         int randomNumber = RandomUtils.nextInt(START_NUMBER_IN_RANGE, END_NUMBER_IN_RANGE);
-
-        // 중복된 값이 아닐 경우, 값 추가
         if (!randomNumbers.contains(randomNumber)) {
             randomNumbers.add(randomNumber);
         }
