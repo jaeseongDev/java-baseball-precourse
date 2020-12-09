d940ba17c27db8bae77ee57b882af8c6a8bc5f53
jaeseongDev <qkrwotjd1445@naver.com>
2020-12-09 16:36:40 +0900

    feat(Input) : 예외 처리 추가(null, 빈 값, 공백일 경우 예외 처리)

--- src/main/java/baseball/ErrorMessage.java
+++ src/main/java/baseball/ErrorMessage.java
@@ -13,5 +13,5 @@ public class ErrorMessage {
         Balls.VALID_LENGTH_OF_BALLS + "자리의 숫자만 입력해야 합니다.";
     public static final String SHOULD_NOT_INPUT_OVERLAPPED_NUMBER = "중복된 숫자를 입력하시면 안 됩니다.";
     public static final String SHOULD_INPUT_CORRECT_VALUE = "올바른 값을 입력하세요.";
-
+    public static final String SHOULD_NOT_INPUT_BLANK = "공백이나 빈 값이나 null을 입력하시면 안 됩니다.";
 }
--- src/main/java/ui/Input.java
+++ src/main/java/ui/Input.java
@@ -12,6 +12,7 @@ public class Input {
         try {
             System.out.print("숫자를 입력해주세요 : ");
             String numbers = scanner.nextLine();
+            validateNullAndEmptyAndBlank(numbers);
             String[] numbersArr = numbers.split("");
             return BallsFactory.createBalls(numbersArr);
         } catch (IllegalArgumentException e) {
@@ -24,6 +25,7 @@ public class Input {
         try {
             System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
             String input = scanner.nextLine();
+            validateNullAndEmptyAndBlank(input);
             validateGameStatus(input);
             return input;
         } catch (IllegalArgumentException e) {
@@ -40,5 +42,11 @@ public class Input {
             return;
         }
         throw new IllegalArgumentException(ErrorMessage.SHOULD_INPUT_CORRECT_VALUE);
+    }
+
+    private static void validateNullAndEmptyAndBlank(String input) {
+        if (input == null || "".equals(input.trim())) {
+            throw new IllegalArgumentException(ErrorMessage.SHOULD_NOT_INPUT_BLANK);
+        }
     }
 }
