6bc0efdde48b752931136578d24c6b04ec2cdd1c
jaeseongDev <qkrwotjd1445@naver.com>
2020-12-16 17:26:52 +0900

    refactor : Balls라는 일급 컬렉션 객체를 생성해서 리팩토링

---
+++ src/main/java/baseball/domain/Balls.java
@@ -0,0 +1,35 @@
+package baseball.domain;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Balls {
+    private static final int BALLS_VALID_SIZE = 3;
+    private final List<Ball> balls;
+
+    public Balls(List<Ball> balls) {
+        validateBallsSize(balls);
+        validateOverlappedBall(balls);
+        this.balls = new ArrayList<>(balls);
+    }
+
+    public List<Ball> getBalls() {
+        return Collections.unmodifiableList(balls);
+    }
+
+    private void validateBallsSize(List<Ball> inputBalls) {
+        if (inputBalls.size() != BALLS_VALID_SIZE) {
+            throw new IllegalArgumentException("입력한 숫자가 3자리 수여야 한다.");
+        }
+    }
+
+    private void validateOverlappedBall(List<Ball> balls) {
+        int notOverlappedBallsSize = (int) balls.stream()
+            .distinct()
+            .count();
+        if (balls.size() != notOverlappedBallsSize) {
+            throw new IllegalArgumentException("중복된 숫자가 들어가면 안 된다.");
+        }
+    }
+}
--- src/main/java/baseball/domain/Game.java
+++ src/main/java/baseball/domain/Game.java
@@ -7,7 +7,7 @@ import java.util.Scanner;
 import utils.StringUtils;
 
 public class Game {
-    private static final int BALLS_VALID_SIZE = 3;
+
     public void start(Scanner scanner)  {
         try {
             getInputBalls(scanner);
@@ -17,36 +17,19 @@ public class Game {
         }
     }
 
-    private List<Ball> getInputBalls(Scanner scanner) {
+    private Balls getInputBalls(Scanner scanner) {
         try {
             String input = InputView.inputNumbers(scanner);
-            validateInputBalls(input);
             String[] numbers = input.split("");
             List<Integer> convertedNumbers = StringUtils.toInts(numbers);
             List<Ball> balls = new ArrayList<>();
             for (int number : convertedNumbers) {
                 balls.add(new Ball(number));
             }
-            validateOverlappedBall(balls);
-            return balls;
+            return new Balls(balls);
         } catch (IllegalArgumentException e) {
             System.out.println(e.getMessage());
             return getInputBalls(scanner);
-        }
-    }
-
-    private void validateInputBalls(String input) {
-        if (input.length() != BALLS_VALID_SIZE) {
-            throw new IllegalArgumentException("입력의 글자 수가 3자리 수여야 한다.");
-        }
-    }
-
-    private void validateOverlappedBall(List<Ball> balls) {
-        int notOverlappedBallsSize = (int) balls.stream()
-            .distinct()
-            .count();
-        if (balls.size() != notOverlappedBallsSize) {
-            throw new IllegalArgumentException("중복된 숫자가 들어가면 안 된다.");
         }
     }
 }
